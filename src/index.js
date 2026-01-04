16d9aee793d4cacb638e50e689061ea618cf9052
ruthlee2376-dot <ruthlee2376@gmail.com>
2026-01-04 14:47:50 +0000

    Added basic form handling and added typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "South African recipe",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
